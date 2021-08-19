e46e1aad1c16cbd66aa30152a81a58c2aed39a0c
Hamze Ahmed <hamzeahmedx@hotmail.com>
2021-08-19 17:56:54 +0300

    ejs installed and templete setup

--- app.js
+++ app.js
@@ -2,10 +2,28 @@ const express = require('express');
 const myBodyParser = require("body-parser");
 
 const app = express()
+app.set('view engine', 'ejs');
 app.use(express.urlencoded({extended: true}));
 app.get('/',(req,res)=>{
-    res.sendFile(__dirname+"/index.html")
+    const tody = new Date();
+    const currentDay = tody.getDay();
+    var myday = ""; 
+    switch(currentDay){
+        case 1:
+            myday = "Monday";
+            break;
+        case 2:
+            myday = "Tuesday";
+            break;
+        case 3:
+            myday = "Wednesday";
+            break;
+        case 4:
+            myday = "Thursday ";
+            break
+    }
+    res.render('list', {kindaDay: myday})
 });
 app.listen(3000, ()=>{
-    console.log('welcome ')
-})
+    console.log('welcome ');
+})  
